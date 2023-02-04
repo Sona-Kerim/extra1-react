6a9604a0a920ba831eceea09e9a8dfafa3b8e94f
Sona-Kerim <kerimlisona1997@gmail.com>
2023-02-04 16:56:04 +0400

    extra tapsiriq

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+const characters = [
+  "Sona",
+  "Eli",
+  "Veli",
+  "Ehmed",
+  "Gulnar"
+];
 
 function App() {
+  const [index, setIndex] = useState(0);
+
+  const func= () => {
+    if (index === characters.length - 1) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={func}>Next</button>
+      <p>{characters[index]}</p>
     </div>
   );
 }
